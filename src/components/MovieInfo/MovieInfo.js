aa76439c893a4187a4b090ec67a27e0f2e8d293f
SK1LL32 <myroslav191@gmail.com>
2022-10-24 17:53:44 +0200

    Install Commit

--- src/components/MovieInfo/MovieInfo.js
+++ src/components/MovieInfo/MovieInfo.js
@@ -1,25 +1,16 @@
-import {useNavigate, useParams} from "react-router-dom";
-import {useEffect} from "react";
+import {useNavigate} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
 
 import {photosImages} from "../../configs";
-import {movieActions} from "../../redux";
 
-const MovieInfo = () => {
 
-    const {id} = useParams();
-
+const MovieInfo = ({next}) => {
     const navigate = useNavigate();
 
     const {movie, genres} = useSelector(state => state.movieReducer);
 
     const dispatch = useDispatch();
 
-    dispatch(movieActions.show(false))
-
-    useEffect(() => {
-        if (id) dispatch(movieActions.getMovie(id))
-    }, [id])
 
     const Genre = (id) => {
         const genre = genres?.genres?.find(value => value.id === id)
@@ -29,31 +20,28 @@ const MovieInfo = () => {
     return (
         <div>
 
-            {movie ?
+
                 <div className={'movie-wrapper'}>
 
                     <div>
-                        <img src={photosImages + movie.poster_path} alt={movie.title}/>
+                        <img src={photosImages + next.poster_path} alt={next.title}/>
                     </div>
                     <div className={'movie-container_content'}>
-                        <h2>{movie?.original_title}</h2>
+                        <h2>{next?.original_title}</h2>
 
                         <div>
                             <span>Genre </span>
                             {
-                                movie?.genres?.map(genre => <span key={genre.id}> {Genre(genre.id)}</span>)
+                                next?.genres?.map(genre => <span key={genre.id}> {Genre(genre.id)}</span>)
                             }
                         </div>
-                        <div><p>Released:<span>{movie?.release_date}</span></p></div>
-                        <div><p>Rating: <span>{movie?.vote_average}</span></p></div>
-                        <div><p>Runtime: <span>{movie?.runtime}</span></p></div>
-                        <div><p>Overview: <span>{movie?.overview}</span></p></div>
+                        <div><p>Released:<span>{next?.release_date}</span></p></div>
+                        <div><p>Rating: <span>{next?.vote_average}</span></p></div>
+                        <div><p>Runtime: <span>{next?.runtime}</span></p></div>
+                        <div><p>Overview: <span>{next?.overview}</span></p></div>
                     </div>
 
                 </div>
-                :
-                <h2 className={"no_info"}>No info</h2>
-            }
 
 
 
--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,10 +1,19 @@
-import {Link} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect} from "react";
 
 import {StarsRating} from "../StarsRating/StarsRating";
+import {movieActions} from "../../redux";
 import {photosImages} from "../../configs";
 import css from './movie.module.css';
+import {Link} from "react-router-dom";
+import {MovieInfo} from "../MovieInfo/MovieInfo";
 
 const MoviesList = ({movie}) => {
+    const {next} = useSelector(state => state.movieReducer);
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(movieActions.getAllMovie)
+    }, [])
 
     return (
 
@@ -17,6 +26,11 @@ const MoviesList = ({movie}) => {
             <p>{movie?.overview} </p>
             <div className={css.rating}>
                 <StarsRating rating={movie?.vote_average}/>
+            </div>
+            <div>
+                {
+                    next?.results?.map((next, index) => <MovieInfo key={index} next={next}/>)
+                }
             </div>
         </div>
     );
